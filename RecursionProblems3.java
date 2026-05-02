94e105d8b9f241d63d2306290d7c20cd33c77e16
Aaron123441 <tatumjraaron@gmail.com>
2026-05-02 05:38:20 +0000

    Recursion Problems

---
+++ RecursionProblems3.java
@@ -0,0 +1,13 @@
+public class RecursionProblems3 {
+    public static int strCount(String str, String sub) {
+   if (str.length() < sub.length()) {
+      return 0;
+   }
+
+   if (str.startsWith(sub)) {
+      return 1 + strCount(str.substring(sub.length()), sub);
+   }
+
+   return strCount(str.substring(1), sub);
+    }
+}
